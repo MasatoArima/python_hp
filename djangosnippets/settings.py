726502a2a7d32c8e4aa11579a9138b3d8da02a92
MasatoArima <afoc.0707@gmail.com>
2021-09-30 22:18:15 +0900

    main

--- djangosnippets/settings.py
+++ djangosnippets/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from .local_settings import SECRET_KEY
+from local_settings import SECRET_KEY
 from pathlib import Path
 import os
 
